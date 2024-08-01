90d5f26bc92f119895c4703960e41408d1c2ee18
chereshan <55876010+chereshan@users.noreply.github.com>
2024-08-01 16:15:21 +0700

    minor textbook fixes

--- server-textbook-express.js
+++ server-textbook-express.js
@@ -18,13 +18,15 @@ app.get('/textbook', (req, res) => {
         res.render('textbook',
             {
                 textbooks: fs.readdirSync(__dirname + '/public/textbook'),
-                title: 'Учебники'
+                title: 'Учебники',
+                mockshop: false
             }
         )
     }
 )
 //todo: написать app.get для хедера
 //todo: экономный поиск title
+//todo: рассортироать app.get-запросы как полагается
 app.get('/textbook/[^/]+', (req, res) => {
     // console.log(req)
     let spec_textbook_name=req.originalUrl.replace('/textbook/', '')
@@ -47,13 +49,29 @@ app.get('/textbook/[^/]+', (req, res) => {
     }
 )
 
-app.get('/textbook/[^/]+/ch[0-9]+', (req, res) => {
-    // console.log(req)
+app.get('/textbook/[Bb]ootstrap/ch[0-9]+', (req, res) => {
+        // console.log('bootstrap')
         res.render('chapter',
             {
                 body: fs.readFileSync(__dirname+'/public/'+req._parsedUrl.pathname+'.html',
                     { encoding: 'utf8', flag: 'r' }),
                 chapter: true,
+                title: '',
+                bootstrap: true,
+                textbooks: fs.readdirSync(__dirname + '/public/textbook')
+            }
+        )
+    }
+)
+
+app.get('/textbook/[^/]+/ch[0-9]+', (req, res) => {
+    // console.log('chapter')
+        res.render('chapter',
+            {
+                body: fs.readFileSync(__dirname+'/public/'+req._parsedUrl.pathname+'.html',
+                    { encoding: 'utf8', flag: 'r' }),
+                chapter: true,
+                bootstrap: false,
                 title: '',
                 textbooks: fs.readdirSync(__dirname + '/public/textbook')
             }
@@ -61,11 +79,19 @@ app.get('/textbook/[^/]+/ch[0-9]+', (req, res) => {
     }
 )
 
-// app.get('/textbook', (req, res) => res.render('textbook'))
-//
-// //online-market
-// //...
-//
+//mock-shop
+app.get('/mockshop', (req, res) => {
+        res.render('mockshop',
+            {
+                title: 'Mockshop',
+                mockshop: true,
+                bootstrap: true
+            }
+        )
+    }
+)
+
+
 // Пользовательская страница 500
 app.use((err, req, res, next) => {
     console.error(err.message)
@@ -90,4 +116,6 @@ app.listen(port, () => console.log(
     `http://localhost:${port}/textbook/nodejs/ch2; \n`+
     `http://localhost:${port}/textbook/nodejs/ch5; \n`+
     `http://localhost:${port}/textbook/nodejs/ch6; \n`+
+    `http://localhost:${port}/textbook/Bootstrap/ch1;\n`+
+    `http://localhost:${port}/mockshop;\n`+
     `нажмите Ctrl+C для завершения.` ))
